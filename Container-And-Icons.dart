6f0eed596e23ac46b0844d3dab6f742c74edd865
Syam Prasad <syamofficial1212@gmail.com>
2026-07-13 15:01:30 +0530

    Create Container-And-Icons.dart for Flutter app
    This file contains a simple Flutter app with a container displaying an icon and text.

---
+++ Container-And-Icons.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue.shade100,
+        appBar: AppBar(
+          title: Text('MY FIRST APP'),
+        ),
+        body: Center(
+          child: Container(
+              width: 200,
+              height: 300,
+              color: Colors.orange,
+              alignment: Alignment.center,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.train,
+                      color: Colors.white,
+                      size: 50,
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Syam Prasad",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ])),
+        ),
+      ),
+    ),
+  );
+}
